66c7affe5f9f76b6340e69b912501a620517a540
onemira <mirabulgogi@gmail.com>
2019-03-26 19:13:27 -0400

    delete button + display all animals

--- src/App.js
+++ src/App.js
@@ -32,11 +32,6 @@ class App extends Component {
   }
 
   render() {
-    // const total = animals.reduce(
-    //   (totalSeen_count, animal) => totalSeen_count + animal.seen_count,
-    //   0
-    // )
-
     return (
       <div className="container">
         <p> All Animals:</p>
@@ -51,34 +46,10 @@ class App extends Component {
             )
           })}
         </ul>
-        <p>
-          {/* {this.state.animals.total(
-            (seen_count, animal => seen_count + animal.seen_count, 0)
-          )} */}
-          the total amount of animal is {this.state.total}!!!!!
-        </p>
+        <p>the total amount of animal is {this.state.total}!!!!!</p>
       </div>
     )
   }
 }
 
 export default App
-
-// map(animal => {
-//   return <Animal key={animal.id} animal={animal} total={this.total} />
-// })
-
-// const formSchema = {
-//   title: 'Animal',
-//   type: 'object',
-//   required: ['species'],
-//   properties: {
-//     species: { type: 'string', title: 'Species', defalut: '' },
-//     last_seen_location: {
-//       type: 'string',
-//       title: 'Last_seen_location',
-//       defalut: ''
-//     },
-//     seen_count: { type: 'string', title: 'Seen_count', defalut: '' }
-//   }
-// }
